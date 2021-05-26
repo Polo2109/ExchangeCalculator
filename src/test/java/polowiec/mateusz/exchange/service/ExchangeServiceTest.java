b13c39795f9804c64e6dbf9a5f3f245a73e026fe
Mateusz <mateusz.polowiec@gmail.com>
2021-05-26 21:52:00 +0200

    Dodanie testu jednostkowego sprawdzającego poprawność obliczeń.

---
+++ src/test/java/polowiec/mateusz/exchange/service/ExchangeServiceTest.java
@@ -0,0 +1,26 @@
+package polowiec.mateusz.exchange.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+import polowiec.mateusz.exchange.dto.ExchangeValues;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+class ExchangeServiceTest {
+
+    @Autowired
+    private ExchangeService exchangeService;
+
+    @Test
+    void isReturnGoodResultValue() {
+        ExchangeValues exchangeValues = new ExchangeValues();
+        exchangeValues.setEurValue(new BigDecimal(100));
+        exchangeValues.setCurrency("PLN");
+        Assert.isTrue((exchangeService.calculate(exchangeValues)).getResultValue().equals(BigDecimal.valueOf(448.39)));
+
+    }
+
+}
